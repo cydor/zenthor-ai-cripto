2ec500b7c713940aa14cb6ec0f1bb10583daf2d0
Cydor <36950659+cydor@users.noreply.github.com>
2025-06-02 18:26:43 +0200

    ♻️  dockerfile (# ENTRYPOINT, CMD python, ci_test_main.py) docker-build.yml(Konténer futtatás CI teszt célból)

--- ci_test_main.py
+++ ci_test_main.py
@@ -8,8 +8,8 @@
 # │││├─┤││││ ├─┘└┬┘     
 # ┴ ┴┴ ┴┴┘└┘o┴   ┴  
 
-# main.py
-# main.py – Egyszerű HTTP healthcheck a konténerhez
+# ci_test_main.py
+# A cél: ✅ Konfig betöltés → 📤 Kiírás → 🚪 Kilépés
 
 import signal
 import time
@@ -22,30 +22,3 @@ if __name__ == "__main__":
     print("✅ Config sikeresen betöltve:")
     print(config)
 
-
-class HealthCheckHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/':
-            self.send_response(200)
-            self.end_headers()
-            self.wfile.write(b"Zenthor AI-Crypto online.")
-
-def start_http_server():
-    server = HTTPServer(('', 8080), HealthCheckHandler)
-    thread = threading.Thread(target=server.serve_forever)
-    thread.daemon = True
-    thread.start()
-    print("✅ HealthCheck szerver elindult a 8080-as porton")
-
-def shutdown_handler(signum, frame):
-    print("🛑 Leállítási jel érkezett. Kilépés...")
-    exit(0)
-
-if __name__ == "__main__":
-    config = load_config()
-    start_http_server()
-    signal.signal(signal.SIGTERM, shutdown_handler)
-    signal.signal(signal.SIGINT, shutdown_handler)
-
-    while True:
-        time.sleep(60)
